c3e5550e1e96e81af491b32e3a26809a150dd8c6
Alt B 2022 <massie.w@gmail.com>
2023-01-09 22:09:03 -0800

    added table of contents text and links.  Still need to work out license text prob a conditional statement somewhere.  Removed bullet for badge to render better.

--- readme_output/README.md
+++ readme_output/README.md
@@ -1,19 +1,25 @@
-# Hello
+# Command Line ReadMe Generator
   ## Description
-  - Hello
+  - Command Line ReadMe Generator
+  ## Table of Contents
+  [Installation](#installation)
+  [Usage](#usage)
+  [License]](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
 
-   
   ## Installation
-  - hello
+  - Node, inquirer, CLI
   ## Usage
-  - hello
+  - Generates a Readme from the command line
   ## License
-  - ![License](https://img.shields.io/badge/License-MIT-lightblue.svg)
+   ![License](https://img.shields.io/badge/License-MIT-lightblue.svg)
   - https://choosealicense.com/licenses/MIT/
-  - undefined
+  - https://choosealicense.com/licenses/MIT/
   ## Contributing
-  - Hello
+  - Contribute
   ## Tests
-  - Hello
+  - TesT
   ## Questions
-  - Hello
+  - Me
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,7 +15,6 @@ function renderLicenseLink(license) {
 } else {
   ("")
 }
-
 }
 // function generateTabOfCon(description) {
 //   let tableOfContents = `## Table of Contents`
@@ -27,21 +26,33 @@ function renderLicenseLink(license) {
 // }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return `https://choosealicense.com/licenses/${license}/`
+  } else {
+    ("")
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## Description
   - ${data.description}
+  ## Table of Contents
+  [Installation](#installation)
+  [Usage](#usage)
+  [License]](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
 
-   
   ## Installation
   - ${data.installation}
   ## Usage
   - ${data.usage}
   ## License
-  - ${renderLicenseBadge(data.license)}
+   ${renderLicenseBadge(data.license)}
   - ${renderLicenseLink(data.license)}
   - ${renderLicenseSection(data.license)}
   ## Contributing
